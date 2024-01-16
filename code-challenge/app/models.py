ccfa7afbf0da35bf0ca2771b032441cd64673da6
Shaddy <shadybett540@gmail.com>
2024-01-16 12:05:49 +0300

    added data to db

--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -6,6 +6,9 @@ class Hero(db.Model):
     __tablename__ = 'hero'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255),nullable=False)
+    super_name = db.Column(db.String(255),nullable=False)
+    
 
 
 # add any models you may need. 
