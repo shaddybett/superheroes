f9c474821e3ed9b3d2e5f086ea9a3c7027e02274
Shaddy <shadybett540@gmail.com>
2024-01-16 12:54:14 +0300

    done

--- code-challenge/app/seed.py
+++ code-challenge/app/seed.py
@@ -1,6 +1,24 @@
 from app import app,db
 from models import Hero
 
+allheros= [  { name: "Kamala Khan", super_name: "Ms. Marvel" },
+  { name: "Doreen Green", super_name: "Squirrel Girl" },
+  { name: "Gwen Stacy", super_name: "Spider-Gwen" },
+  { name: "Janet Van Dyne", super_name: "The Wasp" },
+  { name: "Wanda Maximoff", super_name: "Scarlet Witch" },
+  { name: "Carol Danvers", super_name: "Captain Marvel" },
+  { name: "Jean Grey", super_name: "Dark Phoenix" },
+  { name: "Ororo Munroe", super_name: "Storm" },
+  { name: "Kitty Pryde", super_name: "Shadowcat" },
+  { name: "Elektra Natchios", super_name: "Elektra" }]
 def seed_heros():
-    with app.context():
-        
+    with app.app_context():
+        # hero=Hero(name='Mr Right',super_name='right')
+        # db.session.add(hero)
+        # db.session.commit()
+        for hero in allheros:
+            new_hero=Hero(name=hero.name,super_name=hero.super_name)
+            db.session.add(hero)
+        db.session.commit()
+
+seed_heros()            
