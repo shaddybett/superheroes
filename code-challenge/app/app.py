05a8c17bbf624ed2503f4560d2dbe67209c48f82
Shaddy <shadybett540@gmail.com>
2024-01-16 15:08:15 +0300

    done

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response,jsonify
 from flask_migrate import Migrate
 
 from models import db, Hero
@@ -19,17 +19,34 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+allheros= [
+    {"name": "Kamala Khan", "super_name": "Ms. Marvel"},
+    {"name": "Doreen Green", "super_name": "Squirrel Girl"},
+    {"name": "Gwen Stacy", "super_name": "Spider-Gwen"},
+    {"name": "Janet Van Dyne", "super_name": "The Wasp"},
+    {"name": "Wanda Maximoff", "super_name": "Scarlet Witch"},
+    {"name": "Carol Danvers", "super_name": "Captain Marvel"},
+    {"name": "Jean Grey", "super_name": "Dark Phoenix"},
+    {"name": "Ororo Munroe", "super_name": "Storm"},
+    {"name": "Kitty Pryde", "super_name": "Shadowcat"},
+    {"name": "Elektra Natchios", "super_name": "Elektra"},
+]
+
 @app.route('/')
 def home():
     return ''
+@app.route('/heroes')
+def heroes():
+    return jsonify(allheros)
+
+
 
 @app.route('/add-dumy')
 def add_dumy():
     hero =Hero(name='Mojo Jojo',super_name='Crazy Ape')
     db.session.add(hero)
     db.session.commit()
-    return
-    return
+    return "Hero added"
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555,debug=True)
