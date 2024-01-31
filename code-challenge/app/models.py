739bf4855b1b48fbde81e8feb3e80002adefd2bc
Shaddy <shadybett540@gmail.com>
2024-01-31 13:06:37 +0300

    fixed a typo in seed.py

--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -1,19 +1,3 @@
-# from flask_sqlalchemy import SQLAlchemy
-
-# db = SQLAlchemy()
-
-# class Hero(db.Model):
-#     __tablename__ = 'hero'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(255),nullable=False)
-#     super_name = db.Column(db.String(255),nullable=False)
-    
-
-
-# # add any models you may need. 
-
-
 
 
 
